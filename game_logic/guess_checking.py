733bdfa085e1e080bcf8ea807a1591204e163bd5
modernfuthark <timothys1738@gmail.com>
2020-09-18 14:49:14 -0400

    Updated file to have function

--- game_logic/guess_checking.py
+++ game_logic/guess_checking.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
 """ All functions regarding guess/answer checking """
+
+def Checker(input, master):
+    """ Checker """
+    info = ""
+    cnt = 0
+    for char in input:
+        if char in master:
+            if char == master[cnt]:
+                info += "*"
+            else:
+                info += "#"
+        else:
+            info += "-"
+        cnt += 1
+    if info == "****":
+        return "win"
+    return "".join(reversed(sorted(info)))
